7c1986bdccba49236572d95a1d069e4e8e713167
Son0nline <son0nline@yahoo.com.vn>
2021-04-12 11:01:33 +0700

    update

--- stringtools/app.js
+++ stringtools/app.js
@@ -3,13 +3,14 @@ var app = angular.module('son0nline', []);
 app.controller('validate', function ($scope) {
 	this.sInput = "";
 	this.sResult = "";
+	this.bRPunctuations = false;
 
 	this.gettoken = function () {
 		var that = this;
 		if(this.sInput == undefined){
 			return;
 		}
-		var code = removeVietnameseChar(this.sInput);
+		var code = removeVietnameseChar(this.sInput,this.bRPunctuations);
 		this.sResult = code;
 	};
 });
